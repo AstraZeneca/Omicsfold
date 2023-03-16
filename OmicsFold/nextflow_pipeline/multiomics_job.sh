5b22fd02aaa3a4e35dcbfeff2da640e9feb61205
Ben Sparklin <49563541+bensparklin@users.noreply.github.com>
2023-03-16 09:57:31 -0400

    Edited job submission to be more generalized

--- OmicsFold/nextflow_pipeline/multiomics_job.sh
+++ OmicsFold/nextflow_pipeline/multiomics_job.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#script to submit nextflow multiomics job
+#script to submit nextflow multiomics job to slurm job scheduler
 
 #SBATCH -J multiomics_nextflow
-#SBATCH --mail-type=END,FAIL 
-#SBATCH --mail-user=
 #SBATCH -o multiomics_%j_output.txt
 #SBATCH -e multiomics_%j_error.txt
 #SBATCH -N 1
@@ -12,17 +10,7 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=128GB
 
-module load Miniconda3
-
-source activate nextflow_multiomics
-
 nextflow run multiomics_nextflow.nf.groovy --data $1 --data_labels $2
 
 #example usage on command line
 #sbatch multiomics_job.sh data.rds data_labels.rds
-
-
-
-
-
-
